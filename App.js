8012af00287fdd8b349efef7c17f72b8a589b8b4
Thakur@2001 <thakurdhruv25022001@gmail.com>
2024-08-17 20:19:43 +0530

    git

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = document.createElement("h1");
 // heading.innerHTML = "Hello World From JS";
 // const root = document.getElementById("root");
