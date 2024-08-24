6733c0bd37cb30887385c054a3f8df2dfe2e4231
Thakur@2001 <thakurdhruv25022001@gmail.com>
2024-08-24 15:33:17 +0530

    foodappfirstcommit

--- App.js
+++ App.js
@@ -1,28 +1,68 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const HeadingJsx = () => {
-  return <h1 id="heading"> JSX Element </h1>;
-};
-
-const elem = <h1>React Element</h1>;
-
-const heading = (
-  <div>
-    {elem}
-    <h1>Hello Worlds</h1>
-  </div>
-);
-
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div>
-      {heading}
-      <HeadingJsx />
-      <h1>React-Functional Component</h1>
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://rb.gy/bzr26s" alt="Logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const ResCards = () => {
+  return (
+    <div className="res-Cards">
+      <img
+        className="res-logo"
+        src="https://www.bigsight.jp/visitor/shop/img/carousel-food-square-01.jpg"
+        alt="res-img"
+      />
+      <h3>Hot Bowls</h3>
+      <h4>Burger, Indian, Asian</h4>
+      <h4>4.4 Stars</h4>
+      <h4>40 Minutes</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search-bar">Search</div>
+      <div className="res-Container">
+        <ResCards />
+        <ResCards />
+        <ResCards />
+        <ResCards />
+        <ResCards />
+        <ResCards />
+        <ResCards />
+        <ResCards />
+        <ResCards />
+        <ResCards />
+        <ResCards />
+        <ResCards />
+        <ResCards />
+      </div>
+    </div>
+  );
+};
+const Applayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<Applayout />);
